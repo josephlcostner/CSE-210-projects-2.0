fb8aecc1b6b003d8baec634042b424286fa868a9
josephlcostner <jcostner@byupathway.edu>
2026-01-26 11:28:53 -0700

    First attempt

---
+++ week03/ScriptureMemorizer/Scriptures.cs
@@ -0,0 +1,10 @@
+using System;
+
+class Scriptures
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine($"John 3: 16; For God so Loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
+        Console.ReadLine();
+    }
+}
