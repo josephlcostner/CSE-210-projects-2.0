1e19cb41fd4fef1fdc87163cee27463a2b17affa
josephlcostner <jcostner@byupathway.edu>
2026-01-26 11:52:43 -0700

    Won't run

--- week03/ScriptureMemorizer/Program.cs
+++ week03/ScriptureMemorizer/Program.cs
@@ -1,12 +1,14 @@
 using System;
-
+using Scriptures;
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ScriptureMemorizer Project.");
-        string scriptureQuote = File.ReadAllText("Scriptures.cs");
+        {
+            string scriptureQuote = File.ReadAllText("Scriptures.cs");
         Console.WriteLine(scriptureQuote);
+        }
     }
     
 }
--- week03/ScriptureMemorizer/Scriptures.cs
+++ week03/ScriptureMemorizer/Scriptures.cs
@@ -1,10 +1,13 @@
 using System;
 
-class Scriptures
+namespace Scriptures
 {
-    static void Main(string[] args)
+    public static class Scripture
     {
-        Console.WriteLine($"John 3: 16; For God so Loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life.");
-        Console.ReadLine();
+    private static readonly string[] _quote = 
+    {
+        "John 3: 16; For God so Loved the world, that he gave his only begotten Son, that whosoever believeth in him should not perish, but have everlasting life."
+        
+    };
     }
 }
