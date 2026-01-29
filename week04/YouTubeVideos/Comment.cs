3b71816b560fd8f5f2dc6bbdff96de72b19979c2
josephlcostner <jcostner@byupathway.edu>
2026-01-29 11:56:09 -0700

    Complete: Video Program?

--- week04/YouTubeVideos/Comment.cs
+++ week04/YouTubeVideos/Comment.cs
@@ -4,4 +4,11 @@ public class Comment
 {
     public string Comment1 { get; set; } = "Attempt 1";
     public string Comment2 { get; set;} = "Attempt 2";
+    public string Comment3 { get; set;} = "Attempt 3";
+    public string Comment4 { get; set;} = "Attempt 4";
+    public string Comment5 { get; set;} = "Attempt 5";
+    public string Comment6 { get; set;} = "Attempt 6";
+    public string Comment7 { get; set;} = "Attempt 7";
+    public string Comment8 { get; set;} = "Attempt 8";
+    public string Comment9 { get; set;} = "Attempt 9";
 }
--- week04/YouTubeVideos/Program.cs
+++ week04/YouTubeVideos/Program.cs
@@ -10,6 +10,32 @@ public class Program
         Console.WriteLine(instanceOfVideo1.VideoTape1);
         Comment instanceOfComment1 = new Comment();
         Console.WriteLine(instanceOfComment1.Comment1);
+        Comment instanceOfComment2 = new Comment();
+        Console.WriteLine(instanceOfComment2.Comment2);
+        Comment instanceOfComment3 = new Comment();
+        Console.WriteLine(instanceOfComment3.Comment3);
+
+         Video instanceOfVideo2 = new Video();
+         Console.WriteLine(instanceOfVideo2.VideoTape2);
+         Comment instanceOfComment4 = new Comment();
+        Console.WriteLine(instanceOfComment4.Comment4);
+        Comment instanceOfComment5 = new Comment();
+        Console.WriteLine(instanceOfComment5.Comment5);
+        Comment instanceOfComment6 = new Comment();
+        Console.WriteLine(instanceOfComment6.Comment6);
+
+        Video instanceOfVideo3 = new Video();
+         Console.WriteLine(instanceOfVideo3.VideoTape3);
+         Comment instanceOfComment7 = new Comment();
+        Console.WriteLine(instanceOfComment7.Comment7);
+        Comment instanceOfComment8 = new Comment();
+        Console.WriteLine(instanceOfComment8.Comment8);
+        Comment instanceOfComment9 = new Comment();
+        Console.WriteLine(instanceOfComment9.Comment9);
+        
+        
+         
+
     
     
    
