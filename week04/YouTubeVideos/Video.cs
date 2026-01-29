3c445da9b9cd5dd44cce58530a9158a584ba89ab
josephlcostner <jcostner@byupathway.edu>
2026-01-29 09:48:54 -0700

    sigh

---
+++ week04/YouTubeVideos/Video.cs
@@ -0,0 +1,10 @@
+using System;
+
+
+public class Video
+{
+    string Video_1 = ("My First Video, Joseph Costner, 120");
+    
+    string Video_2 = ("My Second Video, Joseph Costner, 60");
+    string Video_3 = ("My Third Video, Joseph Costner, 90");
+}
