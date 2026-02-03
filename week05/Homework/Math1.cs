b13fd8c2f1e12caeeeefb9e12dc449db08c09d9a
josephlcostner <jcostner@byupathway.edu>
2026-02-02 20:05:09 -0700

    ///

---
+++ week05/Homework/Math1.cs
@@ -0,0 +1,15 @@
+using System;
+namespace math_demo;
+
+public class NextAssignment : Assignment
+{
+    private string _secondstudent = "";
+    public string GetAssignment()
+    {
+        return _assignment;
+    }
+    public void GetAssignment(string assignment)
+    {
+        _secondstudent = secondstudent
+    }
+}
