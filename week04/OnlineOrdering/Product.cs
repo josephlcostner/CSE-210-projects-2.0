bd1cd00066c4658245ca37a6a1647f3903d99261
josephlcostner <jcostner@byupathway.edu>
2026-02-04 12:53:02 -0700

    here we go

--- week04/OnlineOrdering/Product.cs
+++ week04/OnlineOrdering/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.IO.Pipes;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -10,30 +11,32 @@ class Product
     static void Main()
     {
     
-    List<List<string>> ListOfProduce = new List<List<string>>();
-    List<List<int>> listOfPrices = new List<List<int>>();
-    string userInput;
+    List<string> ListOfProduce = new List<string>();
+    List<int> ListOfPrices = new List<int>();
+    string price;
   
     Console.WriteLine("Please enter an item, type exit to quit: ");
 
     while (true)
         {
             Console.Write("Enter item: ");
-            userInput = Console.ReadLine();
+            string userInput = Console.ReadLine();
 
             if (userInput.ToUpper() == "EXIT")
             {
+                Console.WriteLine($"{ListOfProduce}");
+                int totalSum = ListOfPrices.Sum();
+                Console.WriteLine($"{totalSum}");
                 break;
+                
             }
             else if (userInput == "")
             {
-                string Produce = userInput.Substring(2).Trim();
-                if (!string.IsNullOrEmpty(Produce))
-                {
-                    ListOfProduce.Add(Produce);
-                    Console.WriteLine($"{Produce}");
-                    string 
-                }
+                ListOfProduce.Add(userInput);
+                Console.Write("Enter the price: ");
+                price = Console.ReadLine();
+                int.TryParse(price, out int number);
+                ListOfPrices.Add(number);
                 
             }
         }
