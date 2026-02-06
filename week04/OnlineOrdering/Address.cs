307400ee89448dade66a5dfa63f60cef8ca5f068
josephlcostner <jcostner@byupathway.edu>
2026-02-06 10:21:40 -0700

    will it work?

--- week04/OnlineOrdering/Address.cs
+++ week04/OnlineOrdering/Address.cs
@@ -1,14 +1,16 @@
 using System;
 
-public class Address
+namespace Address
 
 
 {
-    private class Home;
-    static void Main4()
+     class Home
+     {
+    public void Main4()
     {
     Console.Write("Please type in your address: ");
     string Location = Console.ReadLine();
     Console.Write($"{Location}");
     }
+     }
 }
--- week04/OnlineOrdering/Customer.cs
+++ week04/OnlineOrdering/Customer.cs
@@ -4,7 +4,7 @@ namespace Customer
 
 
 {
-    class Person;
+    class Person
     {
     public void Identity()
     {
