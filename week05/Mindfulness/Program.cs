2bc3015db10ef8df545b048f7f01993d5ae9f47f
josephlcostner <jcostner@byupathway.edu>
2026-02-09 16:24:15 -0700

    synce up

--- week05/Mindfulness/Program.cs
+++ week05/Mindfulness/Program.cs
@@ -5,5 +5,35 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the Mindfulness Project.");
+
+        bool showMenu = true;
+        while (showMenu)
+        {
+            showMenu = MainMenu();
+        }
+    }
+    private static bool MainMenu()
+    {
+        Console.Clear();
+        Console.WriteLine("1. Breathing Activity");
+        Console.WriteLine("2. Reflection Activity");
+        Console.WriteLine("3. Listing Activity");
+        Console.WriteLine("4. Exit");
+        Console.Write("Please choose an option: ");
+
+        switch (Console.ReadLine())
+        {
+            case "1":
+                return true;
+            case "2":
+                return true;
+            case "3":
+                return true;
+            case "4":
+                break;
+            default:
+                return true;
+
+        }
     }
 }
