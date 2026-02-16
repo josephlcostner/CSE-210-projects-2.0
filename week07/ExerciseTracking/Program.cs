cd2e98b302616cfd7ca21702e8d267385cded093
josephlcostner <jcostner@byupathway.edu>
2026-02-16 15:46:30 -0700

    final week

--- week07/ExerciseTracking/Program.cs
+++ week07/ExerciseTracking/Program.cs
@@ -1,9 +1,19 @@
 using System;
+using Road;
+using SideWalk;
+using Water;
 
 class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Hello World! This is the ExerciseTracking Project.");
+
+        SwimmingLaps swimmingLaps = new SwimmingLaps();
+        swimmingLaps.Swim();
+        StationaryBikes stationaryBikes = new StationaryBikes();
+        stationaryBikes.Ride();
+        Running running = new Running();
+        running.Run();
     }
 }
---
+++ week07/ExerciseTracking/SwimmingLaps.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Reflection.Metadata.Ecma335;
+
+namespace Water
+{
+    class SwimmingLaps
+    {
+        public void Swim()
+        {
+            int laps, time;
+            DateTime currentDateTime = DateTime.Now;
+            
+            Console.Write("Please enter how many laps you swam today: ");
+            laps = Convert.ToInt32(Console.ReadLine());
+            
+            Console.Write("How long did you swim for in minutes: ");
+            time = Convert.ToInt32(Console.ReadLine());
+
+            int Distance = laps * 50 / 1000;
+            int Speed = (Distance / time) * 60;
+            int Pace = time / Distance;
+                
+            Console.Write($"{currentDateTime}, Running({time}, Distance({laps}), Speed({Speed}), Pace({Pace}))");
+        }
+    }
+}
